94472f32491005b92ae2ab9e34f99c407ee515e5
Maroš Zaťko <hacxman@gmail.com>
2014-07-13 18:27:43 +0200

    more migration fixes

--- scraper.py
+++ scraper.py
@@ -5,23 +5,22 @@ import scraperwiki
 url = "http://www.brno.cz/sprava-mesta/volene-organy-mesta/zastupitelstvo-mesta-brna/clenove-zastupitelstva-mesta-brna/"
 input = scraperwiki.scrape(url)
 
-for rd in input:
-    page = lxml.html.fromstring(rd['html'])
-    for a in page.xpath("//a[starts-with(@href, 'mailto:')]"):
-        name = a.text_content().strip()
-        if name: # extra e-mails on the page have pictures
-            email_ref = a.xpath("@href")[0]
+page = lxml.html.fromstring(input)
+for a in page.xpath("//a[starts-with(@href, 'mailto:')]"):
+    name = a.text_content().strip()
+    if name: # extra e-mails on the page have pictures
+        email_ref = a.xpath("@href")[0]
 
-            party = ""
-            party_list = a.xpath("following-sibling::text()")
-            if party_list:
-                match = re.search(r'\(([^\(\)]+)\)', party_list[0])
-                if match:
-                    party = match.group(1).strip()
+        party = ""
+        party_list = a.xpath("following-sibling::text()")
+        if party_list:
+            match = re.search(r'\(([^\(\)]+)\)', party_list[0])
+            if match:
+                party = match.group(1).strip()
 
-            data = { 'name': name,
-                    'email': email_ref[7:],
-                    'party': party
-                }
-            scraperwiki.sqlite.save(['email'], data)
+        data = { 'name': name,
+                'email': email_ref[7:],
+                'party': party
+            }
+        scraperwiki.sqlite.save(['email'], data)
 
